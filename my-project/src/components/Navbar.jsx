ac149db25feb39f3efb2a9296646a18e5b22c9b9
Phai <phai.339@gmail.com>
2024-11-01 14:46:13 +0700

    up2

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ export default function Navbar() {
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
                 <li><Link to='/products'>Product</Link></li>
-            
+                <li><Link to='/profile into'>profile into</Link></li>
+                <li><Link to='/Order'>Order</Link></li>
+                <li><Link to='/Setting'>Settings</Link></li>
             </ul>
         </nav>
     )
