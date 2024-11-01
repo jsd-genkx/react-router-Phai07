8e5e8eaef547715842cd27c2a07b6b46525b7a1a
Phai <phai.339@gmail.com>
2024-11-01 12:47:11 +0700

    react router

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <>
+      <div>
+        <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/contact' element={<Contact />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 
